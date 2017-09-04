03829697ed49950a6c0ad45f1d5998bbdba458f7
Almir Vuk <almir.vuk@outlook.com>
2017-09-04 09:15:01 +0200

    ios - facebook - auth

--- XF.Auth.Demo/XF.Auth.Demo.iOS/AppDelegate.cs
+++ XF.Auth.Demo/XF.Auth.Demo.iOS/AppDelegate.cs
@@ -21,6 +21,8 @@ namespace XF.Auth.Demo.iOS {
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options) {
             global::Xamarin.Forms.Forms.Init();
+            global::Xamarin.Auth.Presenters.XamarinIOS.AuthenticationConfiguration.Init();
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
